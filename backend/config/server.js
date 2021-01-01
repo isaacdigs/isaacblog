2876450bb4b2cb6086ad403b1d828265f80ef784
isaacdigs <pk07057@gmail.com>
2021-01-01 16:51:37 +0900

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8058949159b311571ef1c4e1dbd97aea'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3f661ec0-7416-4f96-a83b-ec2c84925f40'
+};
